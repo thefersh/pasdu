d34e3aa3760679ec7568a8d736f4303e85f65ea9
theferch <thefersh24@gmail.com>
2021-01-24 08:29:34 -0400

    se solucionoproblema al recargar app 'dev'

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<any> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 4000);
