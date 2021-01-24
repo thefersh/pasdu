9a3fc763a20bd1df97a0dcd2ab3dc572c9311bd5
theferch <thefersh24@gmail.com>
2021-01-23 21:08:25 -0400

    test2

--- dist/index.js
+++ dist/index.js
@@ -2,24 +2,11 @@ const express = require('express');
 const { PORT, HOST } = require('./config');
 const cors = require('cors');
 const app = express();
-const sse = require('sse-nodejs');
 const path = require('path');
 
 app.use(cors());
 
 app.use(express.static(path.resolve(__dirname, '../static/')))
-app.get('/sse', (req, res) => {
-    var s = sse(res);
-    s.sendEvent('time', function () {     
-        return (new Date).getTime();
-    },1000);
-
-    s.disconnect( () => {
-        console.log("disconnected");
-    });
-
-});
-
 
 
 app.listen(PORT, HOST, () => console.log(`Server runing on port ${PORT}`));
