76ad0106dc978c3b407a7dea9eadce75370d355c
theferch <thefersh24@gmail.com>
2021-01-25 18:48:19 -0400

    client init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { LogupComponent } from './auth/logup/logup.component';
-import { LayoutComponent } from './layout/layout.component';
-import { IndexTermsComponent } from './terms/index-terms/index-terms.component';
-import { SiteTermsComponent } from './terms/site-terms/site-terms.component';
-import { UserTermsComponent } from './terms/user-terms/user-terms.component';
 
-const routes: Routes = [
-  {path: '', redirectTo: '/app', pathMatch: 'full'},
-  {path: '', component: LayoutComponent, children: []},
-  {path: 'login', component: LoginComponent},
-  {path: 'logup', component: LogupComponent},
-  {path: 'terms', component: IndexTermsComponent, children: [
-    {path: '', component: SiteTermsComponent},
-    {path: 'user', component: UserTermsComponent}
-  ]},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { LayoutComponent } from './layout/layout.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DomService } from './services/dom.service';
-import { LoginComponent } from './auth/login/login.component';
-import { LogupComponent } from './auth/logup/logup.component';
-import { IndexTermsComponent } from './terms/index-terms/index-terms.component';
-import { SiteTermsComponent } from './terms/site-terms/site-terms.component';
-import { UserTermsComponent } from './terms/user-terms/user-terms.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LayoutComponent,
-    LoginComponent,
-    LogupComponent,
-    IndexTermsComponent,
-    SiteTermsComponent,
-    UserTermsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule,
-    NgbModule
+    AppRoutingModule
   ],
-  providers: [
-    DomService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
